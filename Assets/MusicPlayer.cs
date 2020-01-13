c87b8778e0a73b789e036a0bdb1920b7e97ddcdc
prashanth sp <prashanth.sp98@gmail.com>
2020-01-13 15:17:42 +0530

    Added splash scene and music for game, Completed terrain, Added skybox, added music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadNextScene", 5f);
+    }
+    
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    void Update()
+    {
+
+    }
+}
